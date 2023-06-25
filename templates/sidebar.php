241347923cb6ec0e7da7df41aba63d3620f21022
ErikMejiaA <131417967+ErikMejiaA@users.noreply.github.com>
2023-06-24 22:31:09 -0500

    feat :tada: nuevoProjecto plantilla echa en php para iniciar un nuevo proyecto o prueba para campus.

---
+++ templates/sidebar.php
@@ -0,0 +1,26 @@
+<section id="sidebar">
+    <a href="/index.php" class="brand"><i class='bx bxs-planet icon'></i>ESTUDIANTES</a>
+    <ul class="side-menu">
+        <li><a href="/index.php" class="active"><i class='bx bxs-home icon'></i>HOME</a></li>
+        <li class="divider text-center" data-text="MENU"></li>
+        <li>
+            <a href="#"><i class='bx bxs-group icon'></i>ESTUDIANTE <i class='bx bx-chevron-right icon-right'></i></a>
+            <ul class="side-dropdown">
+                <li><a href="/index.php"><i class='bx bxs-user-plus icon'></i>Registrar</a></li>
+                <li><a href="#"><i class='bx bx-table icon'></i>Listar</a></li>
+            </ul>
+        </li>
+        
+        <li class="divider text-center" data-text="table and forms">Table and forms</li>
+        <li><a href="#"><i class='bx bx-table icon'></i> Tables</a></li>
+        <li>
+            <a href="#"><i class='bx bxs-notepad icon'></i> Forms <i class='bx bx-chevron-right icon-right'></i></a>
+            <ul class="side-dropdown">
+                <li><a href="#">Basic</a></li>
+                <li><a href="#">Select</a></li>
+                <li><a href="#">Checkbox</a></li>
+                <li><a href="#">Radio</a></li>
+            </ul>
+        </li>
+    </ul>
+</section>
